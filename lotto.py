54762fc4347b0457c2ff9e99d522e278f61b3dad
Joseph <21023218+jbettenh@users.noreply.github.com>
2021-01-21 20:10:22 +0100

    Initial commit

---
+++ lotto.py
@@ -0,0 +1,54 @@
+import os
+import json
+import random
+
+
+
+def main():
+    print('\n')
+    contest_rules = load_contest()
+    play = input('Which game would you like to play?')
+
+    if play == 'Megaball':
+        game = contest_rules["contests"][0]['name']
+        ball_amount = int(contest_rules["contests"][0]['amount']) + 1
+        special_ball = contest_rules["contests"][0]['extra']
+        first_ball = contest_rules["contests"][0]['start']
+        last_ball = contest_rules["contests"][0]['end']
+        first_special = contest_rules["contests"][0]['extra_start']
+        last_special = contest_rules["contests"][0]['extra_end']
+
+    elif play == 'Powerball':
+        ball_amount = int(contest_rules["contests"][1]['amount']) + 1
+
+    else:
+        print('Pick a game.')
+
+
+    print(f'Loading config... for {game}')
+    print('Printing ticket.....\n')
+
+    for ball in range(1, ball_amount):
+        num = random.randint(first_ball, last_ball)
+        print(f'Number {ball} is {num}')
+
+    for special in range(special_ball):
+        special_num = random.randint(first_special, last_special)
+        print(f'{game}  is {special_num}')
+
+
+def load_contest():
+    directory = os.path.dirname(__file__)
+    filename = os.path.join(directory, 'contest_config.json')
+
+    if not os.path.exists(filename):
+        return {}
+
+    with open(filename, 'r', encoding='utf-8') as fin:
+        return json.load(fin)
+
+
+
+
+if __name__ == '__main__':
+    main()
