4d2df20a86dfd9b985c1d7272085063b4f221ab5
Joseph <21023218+jbettenh@users.noreply.github.com>
2021-01-22 07:44:45 +0100

    Added function to get numbers

--- lotto.py
+++ lotto.py
@@ -5,19 +5,35 @@ import random
 
 def main():
     print('\n')
-    contest_rules = load_contest()
-    play = input('Which game would you like to play? ').lower()
 
-    if play == 'megaball':
+    play = input('Which game would you like to play? ').lower()
+    gen_lotto_numbers(play)
+
+
+def load_contest():
+    directory = os.path.dirname(__file__)
+    filename = os.path.join(directory, 'contest_config.json')
+
+    if not os.path.exists(filename):
+        return {}
+
+    with open(filename, 'r', encoding='utf-8') as fin:
+        return json.load(fin)
+
+
+def gen_lotto_numbers(choice):
+    contest_rules = load_contest()
+
+    if choice == 'megaball':
         game = contest_rules['contests'][0]['name']
-        ball_amount =  contest_rules['contests'][0]['amount'] + 1
+        ball_amount = contest_rules['contests'][0]['amount'] + 1
         special_ball = contest_rules['contests'][0]['extra']
         first_ball = contest_rules['contests'][0]['start']
         last_ball = contest_rules['contests'][0]['end']
         first_special = contest_rules['contests'][0]['extra_start']
         last_special = contest_rules['contests'][0]['extra_end']
 
-    elif play == 'powerball':
+    elif choic == 'powerball':
         game = contest_rules['contests'][1]['name']
         ball_amount = contest_rules['contests'][1]['amount'] + 1
         special_ball = contest_rules['contests'][1]['extra']
@@ -39,17 +55,6 @@ def main():
     for special in range(special_ball):
         special_num = random.randint(first_special, last_special)
         print(f'{game}  is {special_num}')
-
-
-def load_contest():
-    directory = os.path.dirname(__file__)
-    filename = os.path.join(directory, 'contest_config.json')
-
-    if not os.path.exists(filename):
-        return {}
-
-    with open(filename, 'r', encoding='utf-8') as fin:
-        return json.load(fin)
 
 
 if __name__ == '__main__':
