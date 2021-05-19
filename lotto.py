bb9e1ba1bf0e31973c4dbfc926ca7edf82ed6a19
Joseph <21023218+jbettenh@users.noreply.github.com>
2021-05-19 19:59:19 +0200

    Added EuroJackpot

--- lotto.py
+++ lotto.py
@@ -7,8 +7,12 @@ def main():
     print('\n')
 
     play = input('Which game would you like to play? ').lower()
-    picks = gen_lotto_numbers(play)
-    print_ticket(picks)
+    amount_tickets = int(input('How many tickets would you like?'))
+
+    while amount_tickets != 0:
+        picks = gen_lotto_numbers(play)
+        print_ticket(picks)
+        amount_tickets -= 1
 
 
 def load_contest():
@@ -44,6 +48,14 @@ def gen_lotto_numbers(choice):
         first_special = contest_rules['contests'][1]['extra_start']
         last_special = contest_rules['contests'][1]['extra_end']
 
+    elif choice == 'eurojackpot':
+        game = contest_rules['contests'][2]['name']
+        ball_amount = contest_rules['contests'][2]['amount'] + 1
+        special_ball = contest_rules['contests'][2]['extra']
+        first_ball = contest_rules['contests'][2]['start']
+        last_ball = contest_rules['contests'][2]['end']
+        first_special = contest_rules['contests'][2]['extra_start']
+        last_special = contest_rules['contests'][2]['extra_end']
     else:
         print('Pick a game.')
 
